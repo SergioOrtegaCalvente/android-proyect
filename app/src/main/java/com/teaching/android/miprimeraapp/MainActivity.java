1b29491e3c74c8024c3438732483b84f5dd55e90
MEERY\mery <sergio.93.oc@gmail.com>
2018-05-02 10:27:11 +0200

    Escribir log en onStart, onResume, onPause...

--- app/src/main/java/com/teaching/android/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/teaching/android/miprimeraapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.teaching.android.miprimeraapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,7 +21,39 @@ public class MainActivity extends AppCompatActivity {
         valor1=findViewById(R.id.valor1);
         valor2=findViewById(R.id.valor2);
         resultado=findViewById(R.id.resultado);
+        Log.d("MainActivity","onCreate");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity","onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity","onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity","onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity","onResume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity","onDestroy");
+    }
+
     public void sumar(View view){
         String valor1=this.valor1.getText().toString();
         String valor2=this.valor2.getText().toString();
